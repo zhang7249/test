f5611510293bf7353749c20ab14ac5fe63765fff
zhang7249 <724914749@qq.com>
2018-09-11 16:55:48 +0800

    首次提交

---
+++ zzy/src/main/java/com/zzy/zzy/ZzyApplication.java
@@ -0,0 +1,12 @@
+package com.zzy.zzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzyApplication.class, args);
+	}
+}
